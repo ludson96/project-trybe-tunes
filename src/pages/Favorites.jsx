2c6bd7478ce843e647498d7a0f445d88bc03a852
Ludson <ludson_ps27@hotmail.com>
2022-08-03 20:15:33 -0300

    Requisito 13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
-  render() {
-    return (
-      <div data-testid="page-favorites">
-        <Header />
-        Favorites
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      musicasFavoritas: '',
+      loading: true,
+    };
   }
+
+  componentDidMount() {
+    this.favoritMusics();
+  }
+
+   favoritMusics = async () => {
+     this.setState({ loading: true });
+     const data = await getFavoriteSongs();
+     this.setState({
+       musicasFavoritas: data,
+       loading: false,
+     });
+   }
+
+   render() {
+     const { musicasFavoritas, loading } = this.state;
+     return (
+       <div data-testid="page-favorites">
+         <Header />
+         {loading
+           ? <span>Carregando...</span> : (
+             <div>
+               <MusicCard content={ musicasFavoritas } />
+               {/* {this.teste()} */}
+             </div>
+           )}
+       </div>
+     );
+   }
 }
 
 export default Favorites;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
-  render() {
-    return (
-      <div data-testid="page-profile">
-        <Header />
-        Profile
-      </div>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      infoUser: [],
+      loading: true,
+    };
   }
+
+   componentDidMount = async () => {
+     const data = await getUser();
+     this.setState({
+       infoUser: data,
+       loading: false,
+     });
+     console.log(data);
+   }
+
+   render() {
+     const { loading, infoUser } = this.state;
+     return (
+       <div data-testid="page-profile">
+         <Header />
+         {loading
+           ? <span>Carregando...</span>
+           : (
+             <div>
+               <img
+                 src={ infoUser.image }
+                 alt={ `Foto de ${infoUser.name}` }
+                 data-testid="profile-image"
+               />
+               Nome:
+               <p>{infoUser.name}</p>
+               Email:
+               <p>{infoUser.email}</p>
+               Descrição:
+               <p>{infoUser.description}</p>
+               <Link to="/profile/edit">Editar perfil</Link>
+             </div>
+           )}
+       </div>
+     );
+   }
 }
 
 export default Profile;
